a818a13967a81f623db925f26bdeb23de7426b4a
solophone <solophone@aliyun.com>
2020-01-06 14:05:11 +0800

    修改数据库连接方式

--- config/connection.ts
+++ config/connection.ts
@@ -1,11 +1,7 @@
 import { createConnection, Connection } from 'typeorm'
 
 async function connect() {
-  const connection: Connection = await createConnection({
-    type: 'mongodb',
-    url: 'mongodb://admin:admin123456@ds145329.mlab.com:45329/react-admin',
-    useUnifiedTopology: true
-  })
+  const connection: Connection = await createConnection()
 }
 
 connect()
