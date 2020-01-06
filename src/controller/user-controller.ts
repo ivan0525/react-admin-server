326f4c161bd73c7ca25258d718483f511fc00a45
solophone <solophone@aliyun.com>
2020-01-06 14:12:50 +0800

    register接口逻辑实现

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,13 +1,33 @@
-import { Body, JsonController, Post } from 'routing-controllers'
+import { Body, JsonController, Post, Get } from 'routing-controllers'
 import { UserService } from '../service/user-service'
 import { User } from '../entities'
+import { genSalt, hashSync } from 'bcrypt'
 @JsonController()
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService) { }
+
+  @Get('/test')
+  test() {
+    return {
+      message: 'test'
+    }
+  }
   @Post('/register')
   async register(@Body() user: User): Promise<any> {
-    console.log(this.userService)
-    const data = await this.userService.addUser(user)
-    return { data }
+    const exitUser = await User.findOne({ username: user.username })
+    if (exitUser) {
+      return {
+        message: '该用户名已被注册',
+        result: {}
+      }
+    }
+    // 加密
+    const hash = hashSync(user.password, 10)
+    user.password = hash
+    const result = await this.userService.addUser(user)
+    return {
+      message: '操作成功',
+      result
+    }
   }
 }
