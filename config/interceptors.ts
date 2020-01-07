6c6289c3c0033342287f6e37a95238c5ff1de7b0
solophone <solophone@aliyun.com>
2020-01-07 15:42:34 +0800

    add router interceptors

---
+++ config/interceptors.ts
@@ -0,0 +1,11 @@
+import { Interceptor, InterceptorInterface, Action } from 'routing-controllers'
+
+@Interceptor()
+export class AutoAssignJSONInterceptor implements InterceptorInterface {
+  intercept(action: Action, content: any) {
+    if (typeof content === 'object') {
+      return JSON.stringify(Object.assign({ message: 'ok' }, content))
+    }
+    return JSON.stringify({ message: content })
+  }
+}
--- config/routing.options.ts
+++ config/routing.options.ts
@@ -1,8 +1,12 @@
-import { RoutingControllersOptions } from 'routing-controllers'
+import { RoutingControllersOptions } from 'routing-controllers';
 import * as controllers from '../src/controller'
+import * as middleware from './routing.middlewares'
+import * as interceptors from './interceptors'
 import { dictToArray } from './utils'
 export const routingConfigs: RoutingControllersOptions = {
   controllers: dictToArray(controllers),
+  middlewares: dictToArray(middleware),
+  interceptors: dictToArray(interceptors),
 
   // router prefix
   // e.g. api => http://hostname:port/{routePrefix}/{controller.method}
